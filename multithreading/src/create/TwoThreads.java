8ceee5979a044840b57ac000a8aba92227adda9f
Suddhaswatta <suddhaswatta.maity1@gmail.com>
2021-11-04 17:26:42 +0530

    Learn synchronize

--- multithreading/src/create/TwoThreads.java
+++ multithreading/src/create/TwoThreads.java
@@ -2,12 +2,56 @@ package create;
 
 public class TwoThreads {
 
-    /** Will try to synchronize two threads */
+    private static long counter;
 
-    public static int counter = 0;
+    private static int LOOP = 5;
 
-    
+    public static synchronized void inc() {
+        System.out.println(String.format("Thread %s has called inc method ... \n", Thread.currentThread().getName()));
+        counter++;
+    }
+
+    public static void process() {
+
+        Thread t1 = new Thread(() -> {
+            System.out.println(String.format("Thread %s has started running ... \n", Thread.currentThread().getName()));
+            for (int i = 0; i < LOOP; i++) {
+                inc();
+
+            }
+            System.out.println(String.format(
+                    "Thread %s has stopped ... with value %d\n", Thread.currentThread().getName(),
+                    counter
+            ));
+        }, "Thread-1");
+
+        Thread t2 = new Thread(() -> {
+            System.out.println(
+                    String.format(
+                            "Thread %s has started running ... \n",
+                            Thread.currentThread().getName()
+                    )
+            );
+            for (int i = 0; i < LOOP; i++) {
+                inc();
+            }
+            System.out.println(
+                    String.format("Thread %s has stopped ... with value %d\n",
+                    Thread.currentThread().getName(),
+                    counter));
+        }, "Thread-2");
+
+        t1.start();
+        t2.start();
 
 
-    
+    }
+
+    public static void main(String[] args) {
+        process();
+        System.out.println(counter);
+    }
 }
+
+    
+
